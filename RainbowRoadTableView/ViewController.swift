bd3c43e7456500ed2f3a7b13241dc2f891c3c02e
Omari-hajar <89646158+Omari-hajar@users.noreply.github.com>
2021-12-06 15:35:27 +0300

    edit

--- RainbowRoadTableView/ViewController.swift
+++ RainbowRoadTableView/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var RainbowTableView: UITableView!
+    
+  
+    var colors : [String] = [ "yellow", "pastelBlue", "pastelBlue-1", "pastelBlue-2", "pastelBlue-3","purple-1", "purple",   "green",]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        RainbowTableView.dataSource = self
+        RainbowTableView.delegate = self
+        
+      
     }
+    
+    
 
-
+ 
 }
+
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell  {
+       // let color = colors[Int.random(in: 0..<colors.count)]
+        let color = colors[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+       
+        cell.backgroundColor = UIColor(named: color)
+        
+        return cell
+    }
+}
+
+
+
+
 
